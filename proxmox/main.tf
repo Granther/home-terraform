4df2995a70caf9ec7878e7e7ab0f140b6d19c161
Grant <granther321@gmail.com>
2024-12-06 02:29:41 +0000

    Got cloudinit to deploy correctly on proxmox

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -14,88 +14,56 @@ provider "proxmox" {
  pm_tls_insecure = true
 }
 
-# resource "proxmox_vm_qemu" "k8s-lb-test" {
-#  name       = "k8s-lb-test"
-#  target_node = "splinkus-pve"
-#  clone      = "ubuntu-lts-24-04-template"
-# #  vm_state = "stopped"
-#  cpu_type = "host"
-#  cores      = 2
-#  memory     = 2048
-#  vga {
-#   type = "std"
-#  }
-
-#   os_type   = "cloud-init"
-
-#   # CLoud init settings
-#   ipconfig0 = "ip=192.168.5.40/24,gw=192.168.5.1"
-#   ciuser = "grant"
-#   cipassword = "NA"
-#   sshkeys = "NA"
-
-#   disks {
-#     scsi {
-#       scsi0 {
-#         disk {
-#           storage = "local" #mine is local, yours to be local-lvm
-#           size    = 32
-#         }
-#       }
-#     }
-#   }
-# }
-
-
 resource "proxmox_vm_qemu" "k8s-lb-test" {
   name = "k8s-lb-test"
-  target_node = "splinkus-pve"
-  clone = "ubuntu-lts-24-04-template"
-  
-  # Explicitly set cloud-init configuration
-  clone_wait = 10  # Wait time for clone to complete
-  
-  # Cloud-init specific configurations
-  os_type = "cloud-init"
-  
-  # Network configuration
-  ipconfig0 = "ip=192.168.5.40/24,gw=192.168.5.1"
-  
-  # User configuration
-  ciuser = "grant"
-  
-  # If you want to set a password (not recommended for production)
-  cipassword = var.common_pass
-  
-  # SSH Keys
-  # sshkeys = <<-EOF
-  # "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCvT5eILWKC8aNQxgH6fztcld26znnvu68UK5aS/fip4Eu/dSLjkGYEY6HZsagP56EAZ7SqeNyfJTwfghPyeKYLW72BOrT8XfNf//Ix2sgJzBjKeiXfyJkVDbZs92iiM19q7rhJSk2bOQSSVzyRx3xlixqJIiHMc8Oa+CI3E5rq0ShRIzrXSCYFyiBkxe4GvVVkZ7lLUcA+jn2qboF8EjQsNMV4xsRxWZ15cQXYzYDtH+ol11x1AOfWYPeb7NsOb/kDgCrCRamBj1v5SeHwdF20XWWRVeERbSBNaWR6fvv2vkXDye/IxADZsGgJzwLz8AFkZj8G+z+MmWMaJ8PiHUo7gR9Zws3uLBFxAmAAfFhSN/gozCCsV/Bwnut3rgMQCQudhqeG02dPIhZDMdZ2DERi/zhgfDlJ7aTZpmEmLnkhSAPGj6LlwPgilnmknMP30wCHBkm0uy5K7kkplc4DcNdD153QvG8IMviRgE+aq8KuQMGrTuXIjO2nQzzQHdPy8R8= grant@rocky-x1"
-  # EOF
-  
-  # CPU and Memory
-  cpu_type = "host"
+  target_node = var.target_node
+
+  clone = var.clone_template
+
+  # agent = 1
+  full_clone = true
+  os_type = "cloudinit"
   cores = 2
+  sockets = 1
+  cpu_type = "host"
   memory = 2048
+  boot = "order=ide0;scsi0;net0"
+  scsihw = "virtio-scsi-single"
   
-  # VGA Configuration
+  network {
+    id = 0
+    model = "virtio"
+    bridge = "SrvVlan"
+  }
+
+  disk {
+    slot = "scsi0"
+    size = "32G"
+    emulatessd = true
+    discard = true
+    iothread = true
+    storage = "local"
+  }
+
   vga {
     type = "std"
+    memory = 4
   }
-  
-  # Disk Configuration
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          storage = "local"  # Use local-lvm instead of local
-          size = 32
-        }
-      }
-    }
+
+  ipconfig0 = "ip=${var.vm_static_ip}/24,gw=192.168.5.1"
+
+  disk {
+    type = "cloudinit"
+    slot = "ide0"
+    storage = "local"
   }
-  
-  # Optional: Ensure cloud-init is properly triggered
-  provisioner "local-exec" {
-    command = "qm cloudinit dump ${self.vmid} user | echo 'Cloud-init user data dumped'"
-  }
+
+  # Cloud init
+  ciuser = var.ciuser
+  cipassword = var.cipassword
+  ciupgrade = true
+    
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
 }
--- proxmox/vars.tf
+++ proxmox/vars.tf
@@ -3,6 +3,10 @@ variable "ssh_public_keys" {
     type = list(string)
 }
 
+variable "ssh_key" {
+	type = string
+}
+
 variable "token_id" {
 	description = "Name of token in proxmox, 'root@<auth>!<unique_name>'"
 	type = string
@@ -13,12 +17,32 @@ variable "token_secret" {
     type = string
 }
 
-variable "common_pass" {
+variable "ciuser" {
+	default = "grant"
+	type = string
+}
+
+variable "cipassword" {
 	description = "Common password for VM conf"
 	type = string
 }
 
 variable "pm_api_url" {
 	description = "Server url for proxmox"
+	type = string
+}
+
+variable "clone_template" {
+	default = "ubuntu-lts-24-04-template"
+	type = string
+}
+
+variable "target_node" {
+	default = "splinkus-pve"
+	type = string
+}
+
+variable "vm_static_ip" {
+	default = "192.168.5.50"
 	type = string
 }
